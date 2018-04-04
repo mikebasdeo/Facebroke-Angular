6f57c2ca5dc4b6f435cb9a0240d2473c8db3d1ca
mikebasdeo <mikebasdeo@gmail.com>
2018-04-04 14:14:12 -0400

    Added routing to the navbar links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 
 @NgModule({
@@ -19,13 +24,19 @@ import { BsDropdownModule } from 'ngx-bootstrap';
     NavComponent,
     HomeComponent,
     RegisterComponent
+,
+    MemberListComponent,
+    ListsComponent,
+    ListsComponent
+,
+    MessagesComponent
 ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    BsDropdownModule.forRoot()
-
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
