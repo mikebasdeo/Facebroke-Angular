323917c0e9c0653f9fba69923fcb6bd1c1d1f156
mikebasdeo <mikebasdeo@gmail.com>
2018-04-04 13:15:42 -0400

    Added drop down menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { AlertifyService } from './_services/alertify.service';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
 
   ],
   providers: [
