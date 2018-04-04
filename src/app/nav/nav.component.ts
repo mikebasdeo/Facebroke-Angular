b85be0c10e9670926c1d8c17b81adb4a85ffd61a
mikebasdeo <mikebasdeo@gmail.com>
2018-04-04 14:29:15 -0400

    Active buttons, used routes to move users on login and logout

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +15,7 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   // constructors
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   // methods
   ngOnInit() {
@@ -29,7 +30,9 @@ export class NavComponent implements OnInit {
       // if successful
       this.alertify.success('logged in successfully');
     }, error => {
-        this.alertify.error(error);
+        this.alertify.error('Failed to Login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -38,6 +41,7 @@ export class NavComponent implements OnInit {
     this.authService.userToken = null;
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
